c15de63878cc6c6ce5c86ac1d0d385c3e6196692
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-10-19 13:27:52 +0530

    updated multiple inheritance

--- multiple_inheritance.cpp
+++ multiple_inheritance.cpp
@@ -1,4 +1,44 @@
 #include<iostream>
 using namespace std;
 
-class A
+class M
+{
+    protected:
+        int m;
+    public:
+        void get_m(int x)
+        {
+            m=x;
+        }
+};
+
+class N
+{
+    protected:
+        int n;
+    public:
+        void get_n(int y)
+        {
+            n=y;
+        }
+};
+
+class P:public M,public N
+{
+    public:
+        void display()
+        {
+            cout<<"M= "<<m<<endl;
+            cout<<"N= "<<n<<endl;
+            cout<<"M*N= "<<m*n<<endl;
+        }
+};
+
+int main()
+{
+    P p1;
+    p1.get_m(10);
+    p1.get_n(20);
+    p1.display();
+    return 0;
+}
