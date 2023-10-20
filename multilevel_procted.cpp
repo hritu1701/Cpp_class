aeee18dc33c36322f6e3f472a2cde18bbe3c616a
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-10-20 20:33:51 +0530

    updated inheritance

---
+++ multilevel_procted.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class X
+{
+    protected:
+        int a;
+    public:
+        void getdataX()
+        {
+            cout << "Enter the value of a: " << endl;
+            cin >> a;
+        }
+};
+
+class Y : protected X
+{
+    protected:
+        int b;
+    public:
+        void getdataY()
+        {
+            cout << "Enter the value of b: " << endl;
+            cin >> b;
+        }
+};
+
+class Z : protected Y
+{
+    private:
+        int c, result;
+    public:
+        void getdataZ()
+        {
+            cout << "Enter the value of c: " << endl;
+            cin >> c;
+        }
+        void displaysum()
+        {
+            result = a + b + c;
+            cout << "The result is: " << result << endl;
+        }
+};
+
+int main()
+{
+    Z z1;
+    z1.getdataY();
+    z1.getdataZ();
+    z1.displaysum();
+
+    return 0;
+}
